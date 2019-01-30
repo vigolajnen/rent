239c3da76ec2b14f4bc2b114944a383db9e2b2a5
Andrey Shaurov <andrey.shaurov@gmail.com>
2019-01-30 20:47:15 +0300

    renta -1111

--- source/js/main.js
+++ source/js/main.js
@@ -13,7 +13,21 @@ $(".section-scroll").onepage_scroll({
   animationTime: 1000, // AnimationTime let you define how long each section takes to animate
   pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
   updateURL: false, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-  beforeMove: function (index) {}, // This option accepts a callback function. The function will be called before the page moves.
+  beforeMove: function (index) {
+    var $currentSection = $('.onepage-wrapper section:nth-child(' + index + ')');
+    $currentSection.find('.advantages__item').addClass('adv-animation');
+    $currentSection.find('.advantages__item:nth-child(3)').addClass('advBlock3-animation');
+
+    $currentSection.find('.feedback').addClass('left-animation');
+    $currentSection.find('.feedback__item').addClass('itemBlock');
+    $currentSection.find('.feedback__caption').addClass('itemBlock');
+    $currentSection.find('.slider-task__item.slick-slide').addClass('right-animation');
+    $currentSection.find('.section__box.box').addClass('itemBlock');
+
+
+
+
+  }, // This option accepts a callback function. The function will be called before the page moves.
   afterMove: function (index) {}, // This option accepts a callback function. The function will be called after the page moves.
   loop: false, // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
   keyboard: true, // You can activate the keyboard controls
@@ -23,92 +37,28 @@ $(".section-scroll").onepage_scroll({
   direction: "vertical"
 });
 
-$(window).resize(function () {
-  if (document.body.offsetWidth > 769) {
-    
-  }
-});
-
-
-(function ($) {
-  var input = $('input');
-  var feedbackCaption = $('.feedback__caption');
-  var feedbackText = $('.feedback__item');
-  var feedback = $('#feedback');
-  var form = $('form');
-
-  TweenLite.from(input, 1, {autoAlpha: 0});
-  TweenLite.from(feedbackCaption, 2, {autoAlpha: 0});
-  TweenLite.from(feedbackText, 2, {autoAlpha: 0, delay: 1});
-
-  TweenLite.from(feedback, 1, {
-    x: 200,
-    ease:Power1.easeOut
-  });
-
-  // TweenLite.from(form, 1, {
-  //   x: 400,
-  //   ease: Power1.easeOut
-  // });
-  
-})(jQuery);
-
-// плавный скролл
-
-// $(document).ready(function(){
-//     $("#feedback").on("click", function(e){
-//         var anchor = $(this);
-//         $('html, body').stop().animate({
-//             scrollTop: $(anchor.attr('href')).offset().top
-//         }, 777);
-//         e.preventDefault();
-//         return false;
-//     });
-// });
-
-// $('#slider-task').slick({
-// customPaging: function (slider, i) {
-//   return i + 1;
-// },
-// dots: true,
-// appendDots: $('.slider-task .slider-task__buttons .party-news-dots,
-//   .party.news - slider - btns2.party - news - dots '),
-// });
-
 // слайдеры на главной
 $(document).ready(function () {
   $("#slider-task").slick({
-infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  prevArrow: $('.slider-task__button-prev'),
-  nextArrow: $('.slider-task__button-next'),
-  responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.slider-task__button-prev'),
+    nextArrow: $('.slider-task__button-next'),
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+    ]
   })
 });
